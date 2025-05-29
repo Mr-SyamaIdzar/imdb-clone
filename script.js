945cd0377c4101bcff650e3ec21e124865905e9c
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-05-29 11:46:59 +0700

    Fungsional sidebar

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const searchBtn = body.querySelector(".search-box");
 const modeSwitch = body.querySelector(".toggle-switch");
 const modeText = body.querySelector(".mode-text");
 
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
 modeSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    modeText.textContent = "Light Mode"
+  } else {
+    modeText.textContent = "Dark Mode"
+  }
 });
